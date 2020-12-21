1283d61a94c116111853343832fc2550e105533e
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2020-12-21 15:04:41 +0530

    defined Data model

--- src/data/Data.java
+++ src/data/Data.java
@@ -3,5 +3,15 @@
  */
 package data;
 
+import java.util.HashMap;
+
 public class Data {
+
+    public String clientId;
+    public HashMap<String, String> longToShortUrlMap;
+    
+    public Data(String clientId) {
+        this.clientId = clientId;
+        this.longToShortUrlMap = new HashMap<>();
+    }
 }
