7ce3316c4384d25dd55f68939bb408cc04bd30f9
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2020-12-21 15:29:20 +0530

    code refactor

--- src/datamanager/DataManager.java
+++ src/datamanager/DataManager.java
@@ -12,6 +12,7 @@ public class DataManager {
     private static DataManager dataManager;
     public static HashMap<String, Data> clientDataMap;
     public static HashMap<String, String> shorttoLongUrlMap;
+    private static int longUrlId;
 
     public static DataManager getInstance(){
         if(dataManager == null){
@@ -21,6 +22,7 @@ public class DataManager {
     }
 
     private static void init(){
+        longUrlId = 1234;
         dataManager = new DataManager();
         clientDataMap = new HashMap<String, Data>();
         shorttoLongUrlMap = new HashMap<String, String>();
