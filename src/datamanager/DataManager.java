40e067c88c1a265382025e683206616833398efd
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2020-12-21 15:16:29 +0530

    added datamanager

--- src/datamanager/DataManager.java
+++ src/datamanager/DataManager.java
@@ -3,5 +3,44 @@
  */
 package datamanager;
 
+import data.Data;
+
+import java.util.HashMap;
+
 public class DataManager {
+
+    private static DataManager dataManager;
+    public static HashMap<String, Data> clientDataMap;
+    public static HashMap<String, String> shorttoLongUrlMap;
+
+    public static DataManager getInstance(){
+        if(dataManager == null){
+            init();
+        }
+        return dataManager;
+    }
+
+    private static void init(){
+        dataManager = new DataManager();
+        clientDataMap = new HashMap<String, Data>();
+        shorttoLongUrlMap = new HashMap<String, String>();
+    }
+
+    public boolean isLongUrlAlreadyPresentForClient(String longUrl){
+        if(clientDataMap.get(longUrl)!=null){
+            return true;
+        }
+        return false;
+    }
+
+    public String getShortUrlForClient(String clientId, String longUrl){
+       return clientDataMap.get(clientId).longToShortUrlMap.get(longUrl);
+    }
+
+    public static void saveShortUrlToClient(String clientId, String longUrl, String shortUrl){
+        Data data = new Data(clientId);
+        data.longToShortUrlMap.put(longUrl, shortUrl);
+        clientDataMap.put(clientId, data);
+    }
+
 }
